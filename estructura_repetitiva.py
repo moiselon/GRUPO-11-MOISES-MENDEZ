08ce9edea2f2c45269bc189e5e94089d1f71b120
moiselon <moisesmendezm.2001@gmail.com>
2021-02-17 22:42:19 -0400

    Create estructura_repetitiva.py

---
+++ estructura_repetitiva.py
@@ -0,0 +1,70 @@
+'''print('1. Crear un programa que pida números positivos al usuario,y vaya calculandola suma de todos ellos terminará cuando se teclea un número negativo o cero')
+
+print("Suma")
+valor = 0
+while valor !=1:
+    print("Suma de numeros positivos")
+    num1= int(input("Digite su primer numero: "))
+    num2= int(input("Digite su segundo numero: "))
+    if num1 > 0 and num2 > 0:
+        print('el resultado es:',num1 + num2 )
+    else:
+        print("Error no se pueden se dectestan numeros negativos y cero")
+        break'''
+
+'''print('2. Crear un programa que muestre los primeros 10 números pares a partir del producto de (10 x 10).')
+
+for multiplo in range(0,19,2):
+ if multiplo%2==0:
+  print(multiplo)'''
+
+'''print('3. Crear un programa que pida al usuario ingresar un número del 0 al 9 y muestre en pantalla el número en letra. ')
+opcion = 0
+
+while opcion !=1 :
+    numero = int(input("introduzca el numero: "))
+
+    if numero == 0:
+        print("cero")
+
+    elif numero ==1:
+        print("uno")
+
+    elif numero ==2:
+        print("2")
+
+    elif numero == 3:
+      print("tres")
+
+    elif numero == 4:
+        print("4")
+
+    elif numero == 5:
+        print("cinco")
+
+    elif numero == 6:
+        print("6")
+
+    elif numero == 7:
+        print("7")
+
+    elif numero == 8:
+        print("ocho")
+
+    elif numero == 9:
+        print("nueve")
+        break
+print("fin.")'''
+
+
+'''print('4.Crear un programa que muestre las letras de la Z,(mayúscula) a la A (mayúscula, descendiendo). ')
+z = 97
+a = 122
+while z <= a:
+    print(chr(z))
+    z= z-1'''
+
+
+
+
+
